f3df11c4314cafa07fc7ae611422f3558206a17e
Pedro Henrique Machado Bastos <phmachado@gmail.com.br>
2022-04-17 12:42:32 -0300

    Criação do routes

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ import express from 'express';
 
 const app = express();
 
+function getUsername(){
+    return 'diego3g';
+}
+
+const username = getUsername();
+
 app.get('/',(req, res) => {
     return res.send("Hello World");
 
